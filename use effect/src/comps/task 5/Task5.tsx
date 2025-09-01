2253dca448e07b89edd0c2e4a1d11b6b3b86e338
davidlandau770 <dal778770@gmail.com>
2025-09-01 11:54:47 +0300

    fix

--- use effect/src/comps/task 5/Task5.tsx
+++ use effect/src/comps/task 5/Task5.tsx
@@ -6,8 +6,8 @@ export default function Task5() {
     const [print, setPrint] = useState("");
 
     useEffect(() => {
-        console.log("firstName: ", firstName, "lastName:", lastName);
         if (firstName || lastName) {
+            console.log(`firstName: ${firstName}, lastName: ${lastName}`);
             setPrint(`firstName: ${firstName}, lastName: ${lastName}`);
         }
         else {
@@ -17,8 +17,8 @@ export default function Task5() {
   return (
     <div>
         <p>Task5: </p>
-        <input onChange={(e) => setFirstName(e.target.value)}></input>
-        <input onChange={(e) => setLastName(e.target.value)}></input>
+        <input onChange={(e) => setFirstName(e.target.value)} placeholder="first name"></input>
+        <input onChange={(e) => setLastName(e.target.value)} placeholder="last name"></input>
         {print}
     </div>
   )
